d4385c5ecbc2034e5480a281398ef81c8fb84114
foramgajera <f.c.gajera.fg@gmail.com>
2020-12-17 18:47:36 +0530

    create board and square component

---
+++ src/components/Game.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  //it decideto put X or O
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+
+    //select square
+    squares[i] = xO;
+  };
+  return <div></div>;
+};
+
+export default Game;
